bad27c70d9623155628b31650f766bf96adab0d1
atb2192 <atb2192@vt.edu>
2016-10-17 16:13:06 -0400

    quick sort from OpenDSA

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,12 +1,13 @@
 /**
- * {Project Description Here}
+ * Quicksort
  */
 
 /**
  * The class containing the main method.
  *
- * @author {Your Name Here}
- * @version {Put Something Here}
+ * @author Adam Bishop
+ * @author Kevin Zhang
+ * @version 1
  */
 
 // On my honor:
@@ -37,4 +38,46 @@ public class Quicksort {
     public static void main(String[] args) {
         // This is the main file for the program.
     }
+    
+    int partition(short[] A, int left, int right, short pivot) {
+        while (left <= right) {
+            // Move bounds inward until they meet
+            while (A[left] < pivot)
+                left++;
+            while ((right >= left) && (A[right] >= pivot))
+                right--;
+            if (right > left)
+                swap(A, left, right);
+            // Swap out-of-place values
+            }   
+            return left;
+            // Return first position in right partition }
+        }
+    
+        private int findpivot(short[] A, int i, int j) {
+            return (i+j)/2;
+        }
+        
+        void quicksort(short[] A, int i, int j) {
+            // Quicksort
+            int pivotindex = findpivot(A, i, j);
+            // Pick a pivot
+            swap(A, pivotindex, j);
+            // Stick pivot at end
+            // k will be the first position in the right subarray
+            int k = partition(A, i, j-1, A[j]);
+            swap(A, k, j);
+            // Put pivot in place
+            if ((k-i) > 1) quicksort(A, i, k-1);
+            // Sort left partition
+            if ((j-k) > 1)
+                quicksort(A, k+1, j);
+            // Sort right partition }
+        }
+        
+        private void swap(short[] A, int pos1, int pos2) {
+            short temp = A[pos1];
+            A[pos1] = A[pos2];
+            A[pos2] = temp;
+        }
 }
