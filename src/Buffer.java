80c5350b78db5bad72ffd9c40fdd92938010afde
Kevin Zhang <kz4151@vt.edu>
2016-10-19 13:47:08 -0400

    Started on Buffer/BufferPool

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,4 +1,27 @@
-
+/**
+ * Buffer class to hold contents from the file.
+ * 
+ * @author Kevin Zhang
+ * @author Adam Bishop
+ * @version 1.0
+ *
+ */
 public class Buffer {
-
+    private static final int BUFFER_SIZE = 4096;
+    private byte[] data;
+    
+    /**
+     * Constructor 
+     */
+    public Buffer() {
+        data = new byte[BUFFER_SIZE];
+    }
+    
+    /**
+     * Getter for data in the buffer
+     * @return Data in the buffer
+     */
+    public byte[] getData() {
+        return data;
+    }
 }
--- src/BufferPool.java
+++ src/BufferPool.java
@@ -1,4 +1,27 @@
-
+/**
+ * Buffer pool class
+ * 
+ * @author Kevin Zhang
+ * @author Adam Bishop
+ * @version 1.0
+ *
+ */
 public class BufferPool {
-
+    private int bufferCount;
+    private LQueue<Buffer> freeBuffers;
+    private LQueue<Buffer> LRUList;
+    
+    /**
+     * Constructor for buffer pool
+     * @param numberOfBuffers
+     *                 Number of buffers in the pool
+     */
+    public BufferPool(int numberOfBuffers) {
+        bufferCount = numberOfBuffers;
+        for (int i = 0; i < bufferCount; i++) {
+            Buffer dataBuffer = new Buffer();
+            freeBuffers.enqueue(dataBuffer);
+        }
+    }
+    
 }
