89d01b164928abc86060120f089ccbed6d159c7f
atb2192 <atb2192@vt.edu>
2016-10-19 00:13:58 -0400

    Queue for LRU cache

---
+++ src/LQueue.java
@@ -0,0 +1,53 @@
+// Linked queue implementation
+class LQueue<E> {
+    private Link<E> front; // Pointer to front queue node
+    private Link<E> rear; // Pointer to rear queuenode
+    private int size; // Number of elements in queue
+
+    // Constructors
+    LQueue() {
+        init();
+    }
+
+    LQueue(int size) {
+        init();
+    } // Ignore size
+
+    // Initialize queue
+    void init() {
+        front = rear = new Link<E>(null);
+        size = 0;
+    }
+
+    // Put element on rear
+    public boolean enqueue(E it) {
+        rear.setNext(new Link<E>(it, null));
+        rear = rear.next();
+        size++;
+        return true;
+    }
+
+    // Remove and return element from front
+    public E dequeue() {
+        if (size == 0)
+            return null;
+        E it = front.next().element(); // Store the value
+        front.setNext(front.next().next()); // Advance front
+        if (front.next() == null)
+            rear = front; // Last element
+        size--;
+        return it; // Return element
+    }
+
+    // Return front element
+    public E frontValue() {
+        if (size == 0)
+            return null;
+        return front.next().element();
+    }
+
+    // Return queue size
+    public int length() {
+        return size;
+    }
+}
